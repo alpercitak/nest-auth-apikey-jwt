6ae965817d226d40b46da9b0d96e552ac38789c8
Alper Çıtak <citak.alper@gmail.com>
2022-06-06 13:47:14 +0300

    configService for env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './data/users/users.module';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { UsersModule } from '../data/users/users.module';
@@ -11,9 +12,13 @@ import { LocalStrategy } from './strategy/local.strategy';
   imports: [
     PassportModule,
     UsersModule,
-    JwtModule.register({
-      secret: 'jwt-secret',
-      signOptions: { expiresIn: '1d' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRY') },
+      }),
+      inject: [ConfigService],
     }),
   ],
   providers: [AuthService, ApiKeyStrategy, LocalStrategy, JwtStrategy],
