265d051493f9bb234c5ca86e0d808c29ec2cba69
Alper Çıtak <citak.alper@gmail.com>
2026-03-04 09:13:15 +0100

    update @nestjs/passport to v11

--- src/auth/strategy/apikey.strategy.ts
+++ src/auth/strategy/apikey.strategy.ts
@@ -1,19 +1,23 @@
 import { HeaderAPIKeyStrategy } from 'passport-headerapikey';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
-import { AuthService } from '../auth.service';
-import { ConfigService } from '@nestjs/config';
+import type { AuthService } from '../auth.service';
+import type { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ApiKeyStrategy extends PassportStrategy(HeaderAPIKeyStrategy, 'api-key') {
-  constructor(private authService: AuthService, private configService: ConfigService) {
+  constructor(
+    private authService: AuthService,
+    private configService: ConfigService,
+  ) {
     const headerKeyApiKey = configService.get<string>('HEADER_KEY_API_KEY') || '';
+    super({ header: headerKeyApiKey, prefix: '' }, true);
+  }
 
-    super({ header: headerKeyApiKey, prefix: '' }, true, async (apiKey, done) => {
-      if (this.authService.validateApiKey(apiKey)) {
-        done(null, true);
-      }
-      done(new UnauthorizedException(), null);
-    });
+  async validate(apiKey: string): Promise<any> {
+    if (this.authService.validateApiKey(apiKey)) {
+      return true;
+    }
+    throw new UnauthorizedException();
   }
 }
